0dd2610626f8ac6913280eb8e82e194ece8ece6d
Sehee Hong <seaheehong@gmail.com>
2026-02-10 23:52:14 +0900

    feat: 인증 관련 Request DTO에 입력 검증 어노테이션 추가
    LoginRequest, RegisterRequest, ChangePasswordRequest, UpdateProfileRequest에
    @NotBlank, @Email, @Size 등 JSR-303 검증 추가

--- server/src/main/java/com/gugarden/dto/request/ChangePasswordRequest.java
+++ server/src/main/java/com/gugarden/dto/request/ChangePasswordRequest.java
@@ -1,10 +1,16 @@
 package com.gugarden.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class ChangePasswordRequest {
+    @NotBlank(message = "현재 비밀번호를 입력해주세요.")
     private String currentPassword;
+
+    @NotBlank(message = "새 비밀번호를 입력해주세요.")
+    @Size(min = 6, message = "비밀번호는 6자 이상이어야 합니다.")
     private String newPassword;
 }
--- server/src/main/java/com/gugarden/dto/request/UpdateProfileRequest.java
+++ server/src/main/java/com/gugarden/dto/request/UpdateProfileRequest.java
@@ -1,11 +1,14 @@
 package com.gugarden.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class UpdateProfileRequest {
+    @NotBlank(message = "이름을 입력해주세요.")
     private String name;
+
     private String phone;
     private String address;
     private String addressDetail;
